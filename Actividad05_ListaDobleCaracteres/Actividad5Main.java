dea15b035a960548c2ec1272233366cfd5356661
1224100685lojg-web <1224100685.lojg@gmail.com>
2025-10-21 23:05:09 -0600

    Refactor comments and author details in Actividad5Main
    Updated author information and added comments for clarity.

--- Actividad05_ListaDobleCaracteres/Actividad5Main.java
+++ Actividad05_ListaDobleCaracteres/Actividad5Main.java
@@ -6,11 +6,19 @@ package EjerciciosPracticos;
 import java.util.Scanner;
 /**
  *
- * @author Owen_04
+* Clase principal para ejecutar la Actividad5.
+ * Permite al usuario:
+ * - Ingresar una cadena de caracteres.
+ * - Crear una lista doblemente enlazada a partir de la cadena.
+ * - Mostrar la lista original.
+ * - Ordenar alfabéticamente los caracteres y mostrar la lista ordenada.
+
+ * @author Luis Owen Jaramillo Guerrero
  */
 public class Actividad5Main {
 
     /**
+    //Método principal que controla la ejecución del programa.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -18,14 +26,17 @@ public class Actividad5Main {
         Scanner sc = new Scanner(System.in);
         Actividad5 lista = new Actividad5();
 
+        // Solicitar cadena al usuario
         System.out.println("=== Actividad 5 - Lista doblemente enlazada de caracteres ===");
         System.out.print("Ingrese una cadena: ");
         String s = sc.nextLine();
 
+        // Mostrar lista original
         lista.buildFromString(s);
         System.out.println("Lista original:");
         lista.printList();
 
+        // Ordenar la lista alfabéticamente
         lista.bubbleSort();
         System.out.println("Lista ordenada:");
         lista.printList();
